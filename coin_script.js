81ad7ced6cce938f310d0e4993a103acf23ea443
bentleybuilds <bentleybuilds@gmail.com>
2017-08-17 14:24:39 -0600

    corrected syntax on JS line 7

--- coin_script.js
+++ coin_script.js
@@ -4,9 +4,10 @@ var coin;
 function ranFlip(){
   coin = Math.floor((Math.random() * 100) + 1);
   console.log("number generated");
-  if coin <= 50 {alert("HEADS!");
-  $(ol).append("<li>Heads</li>");
-  console.log("if");
+  if (coin <= 50) {
+    alert("HEADS!");
+    $(ol).append("<li>Heads</li>");
+    console.log("if");
 }
   else {
     {alert("TAILS!");
