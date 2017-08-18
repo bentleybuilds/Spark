1ec2991b6b2d57efb00e0345ffbaeb54932c1713
bentleybuilds <bentleybuilds@gmail.com>
2017-08-17 18:59:40 -0600

    moving scripts to .js

--- coin_script.js
+++ coin_script.js
@@ -1,24 +1,23 @@
-<script type="text/javascript">
-  console.log($)
-  console.log("starting")
-  $("#flip").click(function(){
+console.log($)
+console.log("starting")
+$("#flip").click(function(){
 
 
 
-  console.log("button clicked");
-    var coin = Math.floor((Math.random() * 100) + 1);
-    console.log("number generated");
-    if (coin <= 50) {
-      alert("HEADS!");
-      $("ol").append("<li>Heads</li>");
-      console.log("if");
-  } else {
-      alert("TAILS!");
-      $("ol").append("<li>Tails</li>");
-      console.log("else");
+console.log("button clicked");
+  var coin = Math.floor((Math.random() * 100) + 1);
+  console.log("number generated");
+  if (coin <= 50) {
+    alert("HEADS!");
+    $("ol").append("<li>Heads</li>");
+    console.log("if");
+} else {
+    alert("TAILS!");
+    $("ol").append("<li>Tails</li>");
+    console.log("else");
 
-    }
-  });
-  $("#clear").click(function(){
-    $("ol").html("");
-  });
+  }
+});
+$("#clear").click(function(){
+  $("ol").html("");
+});
